42efbebadd8be60fb511ae479a6aa2512ceefb75
Karl Kowalczyk <karl1112@gmail.com>
2024-05-24 23:00:49 -0600

    Updated docker deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 export FLASK_APP=main.py
 flask db init
 flask db migrate -m "initial db"
+flask create-admin
 python -u main.py
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="hkabtrak"
 docker build -t ${app} .
-docker run -d -p 10400:80 \
+docker run -d -p 1473:1473 \
   --name=${app} \
   -v $PWD:/app ${app}
