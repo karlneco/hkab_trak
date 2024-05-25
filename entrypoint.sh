b53415eb76038fe3ce682773320ee8cdf31588ad
Karl Kowalczyk <karl1112@gmail.com>
2024-05-25 00:03:54 -0600

    More docker changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 export FLASK_APP=main.py
 flask db init
 flask db migrate -m "initial db"
+flask db upgrade
+sleep 5
 flask create-admin
 python -u main.py
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ app="hkabtrak"
 sudo docker build -t ${app} .
 sudo docker run -p 1473:1473 \
   --name=${app} \
-  -v $PWD:/app ${app}
+  -v $PWD/instance:/usr/app/instance ${app}
