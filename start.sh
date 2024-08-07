bd47bbf613f887d7ddf93c52c53288afba4ace21
Karl Kowalczyk <karl1112@gmail.com>
2024-08-06 21:01:54 -0600

    fixed instance directory location

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ docker build -t ${app} .
 docker run -p 1473:1473 \
   --name=${app} \
   -e SECRET_KEY='your_secret_key_value' \
-  -v $PWD/instance:/usr/app/instance ${app}
+  -v $PWD/instance:/app/instance ${app}
