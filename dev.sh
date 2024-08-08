500816fc9827e91a378e69094640209cadce6fb6
Karl Kowalczyk <karl1112@gmail.com>
2024-08-07 18:07:33 -0600

    updating dev launch script

--- dev.sh
+++ dev.sh
@@ -1,6 +1,8 @@
 export FLASK_APP=main.py
 export FLASK_ENV=development
-export SECRET_KEY=your_secret_key_value
 
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
 
 flask run --host 0.0.0.0 --port 3000
